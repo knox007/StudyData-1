0a4dca3357ed9a7476a7cd32689bbb17d2fe9de4
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-07-11 02:20:03 +0900

    클래스 화

---
+++ _5,6. dx/_강의용/2. Create Device/DX_CreateDevice(class)/Main.h
@@ -0,0 +1,27 @@
+// Interface for the CMain class.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+class CMain : public CD3DApp
+{
+public:
+
+public:
+	CMain();
+
+	virtual INT		Init();
+	virtual void	Destroy();
+
+	virtual INT		FrameMove();
+	virtual INT		Render();
+
+	virtual LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
+};
+
+
+extern CMain*	g_pApp;
+
+#endif
