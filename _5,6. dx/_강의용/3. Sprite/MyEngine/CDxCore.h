4d423e6c5f07d5f5bd7e67b5e6ca6426b0c51fd4
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-07-14 18:11:46 +0900

    스프라이트

---
+++ _5,6. dx/_강의용/3. Sprite/MyEngine/CDxCore.h
@@ -0,0 +1,62 @@
+//========================================================
+#pragma once
+//========================================================
+#ifndef _DXCORE_H_
+#define _DXCORE_H_
+//========================================================
+class CDxCore
+{
+protected:
+	//-------------------------------
+	//	win
+	HINSTANCE	m_hInst;
+	HWND		m_hWnd;
+	DWORD		m_dwWinStyle;
+	DWORD		m_dwScrWidth;
+	DWORD		m_dwScrHeight;
+	BOOL		m_bShowCursor;
+	BOOL		m_bWindowMode;
+	TCHAR		m_tszClassName[256];
+
+	static LRESULT WINAPI WndProc(HWND, UINT, WPARAM, LPARAM);
+	void	CleanUp();
+	HRESULT	Idle();
+	//-------------------------------
+	//	d3d
+	LPDIRECT3D9				m_pD3D;
+	D3DPRESENT_PARAMETERS	m_D3DPp;
+	LPDIRECT3DDEVICE9		m_pD3DDevice;
+	//-------------------------------
+
+public:
+	//-------------------------------
+	CDxCore();
+	~CDxCore();
+	//-------------------------------
+	//	core
+	BOOL	Create(HINSTANCE hInst);
+	INT		Run();	
+	//-------------------------------
+	//	game
+	virtual INT		Init()		{ return 0; }	
+	virtual INT		Render()	{ return 0; }
+	virtual INT		FrameMove() { return 0; }
+	virtual void	Destroy()	{			}
+	//-------------------------------
+	//	procedure
+	virtual	LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
+	//-------------------------------
+
+
+private:
+	//-------------------------------
+	void RegistWinClass();
+	HRESULT CreateWindows();
+	HRESULT CreateDX();
+	HRESULT OnRender();
+	//-------------------------------
+	
+
+};	//class CDxCore
+//========================================================
+#endif	//	_DXCORE_H_
--- _5,6. dx/_강의용/3. Sprite/MyEngine/Util.h
+++ _5,6. dx/_강의용/3. Sprite/MyEngine/Util.h
@@ -1,13 +1,10 @@
-// Interface for the Utilities.
-//
-////////////////////////////////////////////////////////////////////////////////
+#pragma once
 
-#ifndef _MCUTIL_H_
-#define _MCUTIL_H_
+#ifndef _UTIL_H_
+#define _UTIL_H_
 
 #define SAFE_DELETE(p)       { if(p) { delete (p);     (p)=NULL; } }
 #define SAFE_DELETE_ARRAY(p) { if(p) { delete[] (p);   (p)=NULL; } }
 #define SAFE_RELEASE(p)      { if(p) { (p)->Release(); (p)=NULL; } }
-
 
 #endif
