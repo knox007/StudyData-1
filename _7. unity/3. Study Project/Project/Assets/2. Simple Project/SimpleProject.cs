2de74f588e4a6269d08f9429a26ae2dbbb1e5ce7
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-07-16 10:55:27 +0900

    유니티 수업

---
+++ _7. unity/3. Study Project/Project/Assets/2. Simple Project/SimpleProject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleProject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("-- Start() is called --" );
+        //Debug.Log("-- Start() is called --"); 행 단위 주석.
+		
+	}
+
+    /*  블록 단위 주석 시작.
+	// Update is called once per frame
+	void Update () {
+	
+    
+	}
+    블록 단위 주석 끝.*/
+}
+;
