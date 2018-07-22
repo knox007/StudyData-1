1f36ba17c5ae75b961219e32af57edf8cf3df94e
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-07-22 22:32:59 +0900

    activeself vs activeinhierarchy

---
+++ _7. unity/3. Study Project/Project/Assets/_Reference Study/3. active/activetest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activetest : MonoBehaviour {
+
+    public GameObject _child;
+	// Use this for initialization
+	void Start ()
+    {
+        //_child.SetActive(false);
+
+        print(" activeSelf = " + _child.activeSelf);
+        print(" activeInHierarchy = " + _child.activeInHierarchy);        
+    }
+
+
+}
