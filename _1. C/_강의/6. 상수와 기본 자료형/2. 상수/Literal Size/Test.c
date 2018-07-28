075c5f6d32dd1d8d3efa24e75a0b2f2eafc99f62
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-07-29 01:05:06 +0900

    상수

---
+++ _1. C/_강의/6. 상수와 기본 자료형/2. 상수/Literal Size/Test.c
@@ -0,0 +1,46 @@
+//======================================================
+#pragma warning (disable : 4996)
+#include <stdio.h>
+//======================================================
+/*
+1.	Literal Size
+
+	int 형으로 표현이 가능한 정수형 상수는 int 형으로 메모리 공간에 저장하기로 규정.
+
+	double 형으로 표현이 가능한 정수형 상수는 double 형으로 메모리 공간에 저장하기로 규정.
+
+	문자형 상수의 경우 컴파일러에 의해 정수로 변환되므로 int형으로 표현된다.
+
+*/
+//*
+int main(void)
+{
+	printf("literal int size : %d\n", sizeof(10));
+	printf("literal double size : %d\n", sizeof(3.14));
+	printf("literal char size : %d\n", sizeof('A'));
+
+	return 0;
+}
+//*/
+//======================================================
+
+
+
+//======================================================
+/*
+2.	심볼릭 상수
+*/
+/*
+#define BULLET_SPEED 20
+int main(void)
+{
+	const int BULLET_POW = 100;
+
+	printf("총알의 속도  : %d\n", BULLET_SPEED);
+	printf("총알의 공격력  : %d\n", BULLET_POW);
+
+	return 0;
+}
+
+//*/
+//======================================================
