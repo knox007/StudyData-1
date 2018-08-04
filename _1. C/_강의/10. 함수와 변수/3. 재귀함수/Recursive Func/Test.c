6400da3dec0994ddb2ccca2d31497f64f43b244a
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-04 20:29:04 +0900

    c

---
+++ _1. C/_강의/10. 함수와 변수/3. 재귀함수/Recursive Func/Test.c
@@ -0,0 +1,88 @@
+//======================================================
+#pragma warning (disable : 4996)
+#include <stdio.h>
+//======================================================
+/*
+1.	재귀 함수
+
+	-	함수 내에서 자기 자신을 다시 호출하는 함수.
+
+
+2.	형식
+
+	-	void RecursiveFunc()
+		{
+			printf( "Recursive!!\n" );
+
+			RecursiveFunc();
+		}
+
+	-	원리
+
+		-	함수를 구성하는 명령문은 cpu로 복사가 되어 실행된다.
+
+		-	함수 중간에 다시 자기 자신을 만나면 복사본을 cpu로 넘겨서 실행한다.
+
+3.	주의 사항
+
+	-	재귀함수를 정의할때 탈출조건을 명시할 것.
+		
+*/
+
+
+/*
+void Recursive(int num)
+{
+	if (num <= 0)
+		return;
+
+	printf("Recursive Call!! %d\n", num);
+
+	Recursive(num - 1);
+}
+
+int main(void)
+{
+	Recursive(3);
+
+	return 0;
+}
+//*/
+//======================================================
+/*
+quiz)	입력한 수 이하 정수의 합을 구하는 재귀 함수
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int Recursive(int num)
+{
+	if (num == 0)
+		return num;
+	else
+		return num + Recursive(num - 1);
+}
+
+int main(void)
+{
+	int num = Recursive(100);
+
+	printf("%d\n", num);
+
+	return 0;
+}
+*/
