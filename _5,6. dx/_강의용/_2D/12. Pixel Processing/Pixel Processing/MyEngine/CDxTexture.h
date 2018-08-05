1f8d5b6a0a7b035465102623629e7cd0b6540b23
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-06 07:58:27 +0900

    dx

--- _5,6. dx/_강의용/_2D/12. Pixel Processing/Pixel Processing/MyEngine/CDxTexture.h
+++ _5,6. dx/_강의용/_2D/12. Pixel Processing/Pixel Processing/MyEngine/CDxTexture.h
@@ -5,11 +5,11 @@
 #define _DXTEXTURE_H_
 //========================================================
 class CDxTexture;
-typedef void(CDxTexture::*SetBitsFuncPtr)(DWORD* pdwBits, float fR, float fG, float fB, float fA);
+typedef void(CDxTexture::*SetBitsFuncPtr)(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit );
 //========================================================
 enum eEFFMODE
 {
-	COLOR,
+	COLORSHIFT,
 
 	MONO,
 
@@ -39,11 +39,14 @@ protected:
 	D3DLOCKED_RECT		m_d3dRc;
 	SetBitsFuncPtr		m_pfFunc;
 
-	void SetRGBA_Default(DWORD* pdwBits, float fR, float fG, float fB, float fA);
-	void SetRGBA_Mono(DWORD* pdwBits, float fR, float fG, float fB, float fA);
-	void SetRGBA_InverseColor(DWORD* pdwBits, float fR, float fG, float fB, float fA);
-	void SetRGBA_Gamma(DWORD* pdwBits, float fR, float fG, float fB, float fA);
-	void SetRGBA_BitPlannerSlicing(DWORD* pdwBits, float fR, float fG, float fB, float fA);
+	void SetRGBA_ColorShift(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+	void SetRGBA_Mono(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+	void SetRGBA_InverseColor(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+	void SetRGBA_Gamma(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+	void SetRGBA_BitPlannerSlicing(DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+
+	void SetBits(SetBitsFuncPtr pFunc, DWORD* pdwBits, float fR, float fG, float fB, float fA, int nBit);
+	void SetRGBA(eEFFMODE eMode, float fR = 1.0f, float fG = 1.0f, float fB = 1.0f, float fA = 1.0f,int nBit = 2);
 	
 public:
 	CDxTexture();
@@ -51,13 +54,13 @@ public:
 
 	virtual INT		Create(LPDIRECT3DDEVICE9 pDev, char* sFile, DWORD dwColor = 0xff000000, DWORD dwFormat = D3DFMT_UNKNOWN);
 	virtual void	Destroy();
-	
-	void	SetBits(SetBitsFuncPtr pFunc, DWORD* pdwBits, float fR, float fG, float fB, float fA);
-	void	SetRGBA(eEFFMODE eMode, float fR = 1.0f, float fG = 1.0f, float fB = 1.0f, float fA = 1.0f);
-	void	SetMono();
-	void	SetInverseColor();
-	void	SetGamma();
 
+	void SetEffect_ColorShift(float fR = 1.0f, float fG = 1.0f, float fB = 1.0f, float fA = 1.0f)	{ SetRGBA(COLORSHIFT, fR, fG, fB, fA); }
+	void SetEffect_Mono()	{ SetRGBA(MONO, 0.3f, 0.59f, 0.11f); }//{ SetRGBA(MONO, 0.333f, 0.333f, 0.333f); }
+	void SetEffect_InverseColor()	{ SetRGBA(INVERSECOLOR); }
+	void SetEffect_Gamma(float fR = 1.0f, float fG = 1.0f, float fB = 1.0f, float fA = 1.0f)	{ SetRGBA(GAMMA, 0.5f, 0.5f, 0.5f); }
+	void SetEffect_BitPlannerSlicing(int nBit) { SetRGBA(BITPLANNERSLICING, 0.5f, 0.5f, 0.5f, 1.0f, nBit); }
+	
 public:
 	INT		GetImageWidth();
 	INT		GetImageHeight();
