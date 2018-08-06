e959fcd8411940aebd5eebb5b258a486d407adaf
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-07 00:41:37 +0900

    3d

---
+++ _5,6. dx/_강의용/_3D/MyEngine/Global.h
@@ -0,0 +1,27 @@
+//========================================================
+#pragma warning( disable : 4996)
+//========================================================
+#pragma once
+//========================================================
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "winmm.lib")	//	timeGetTime
+//========================================================
+#include "resource.h"
+#include <windows.h>
+#include <mmsystem.h>				//	timeGetTime
+#include <d3d9.h>
+#include <d3dx9.h>
+//========================================================
+#include <tchar.h>
+#include <stdio.h>
+//========================================================
+#include "Util.h"
+#include "CInputManager.h"
+#include "CDxSprite.h"
+#include "CDxCore.h"
+#include "CDxTexture.h"
+#include "CDxFont.h"
+//========================================================
+#include "CMyGame.h"
+//========================================================
