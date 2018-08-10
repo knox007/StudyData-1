e31d165fd434a0bfe499456100f98f6fa61ada2f
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-10 10:31:31 +0900

    dx

---
+++ _5,6. dx/_강의용/_3D/4. 광원/MyEngine/CMyGame.h
@@ -0,0 +1,44 @@
+//============================================
+#pragma once
+//============================================
+#ifndef _MYGAME_H_
+#define _MYGAME_H_
+//============================================
+#include "Global.h"
+//============================================
+class CMyGame :	public CDxCore
+{
+	//------------------------------------------
+public:
+	CMyGame();
+	virtual ~CMyGame();
+
+	virtual INT		Init();
+	virtual INT		Render();
+	virtual INT		FrameMove();
+	virtual void	Destroy();
+
+	virtual HRESULT	Restore();
+	virtual void	Invalidate();
+
+	virtual LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
+	
+	
+	TCHAR			m_tszMessage[256];
+
+	//------------------------------------------
+	HRESULT	InitVB();
+	void SetMatrix();
+	void SetMaterial();
+	void SetLight();
+	LPDIRECT3DVERTEXBUFFER9 m_pVB;	// 정점 버버
+	//------------------------------------------
+
+
+
+};// class CMyGame :	public CDxCore
+
+extern CMyGame*	g_pApp;
+//============================================
+#endif // _MYGAME_H_
+//============================================
