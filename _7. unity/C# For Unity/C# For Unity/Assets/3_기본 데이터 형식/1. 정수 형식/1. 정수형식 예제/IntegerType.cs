490903188bd748da99f801e0a1aaa859f127e6f5
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-21 17:15:29 +0900

    u

---
+++ _7. unity/C# For Unity/C# For Unity/Assets/3_기본 데이터 형식/1. 정수 형식/1. 정수형식 예제/IntegerType.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntegerType : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        sbyte a = -10;
+        //sbyte a = -500000000000;
+        byte b = 40;
+
+        print(a);
+        print(b);
+
+        short c = -30000;
+        ushort d = 60000;
+
+        print(c);
+        print(d);
+
+        //int e = -1000_0000;   //  자릿수 구분자 c# 7.0에서 지원 
+        int e = -10000000;
+        //int f = 3_0000_0000;
+        int f = 300000000;
+
+        print(e);
+        print(f);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ _7. unity/C# For Unity/C# For Unity/Assets/3_기본 데이터 형식/1. 정수 형식/4. 오버플로우/OverFlow.cs
@@ -0,0 +1,48 @@
+﻿/*
+--------------------------
+1.	오버 플로우
+--------------------------
+
+	1.	변수는 자료형에 따라 관리할 수 있는 데이터의 범위가 정해진다.
+
+	2.	오버 플로우
+
+		-	변수가 관리할 수 있는 범위를 넘어서는 크기의 값을 저장하게 되면 발생하는 현상.
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverFlow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        byte a = byte.MaxValue; //  255 -> 1111 1111
+
+        print(a);
+
+        ++a;    //  1111 1111 + 0000 0001 = 0001 0000 0000
+
+                //  1바이트는 8개의 비트만 관리할수 있으므로
+
+                //  자릿수가 올라간 왼쪽 비트는 버려지고 오른쪽 8개의 비트만 관리함.
+        
+        print(a);
+        
+
+        sbyte b = sbyte.MinValue;   //  -128    ->  1000 0000
+
+        print(b);
+
+        --b;    //  1000 0000 - 0000 0001 = 0111 1111 = 127
+
+        print(b);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
