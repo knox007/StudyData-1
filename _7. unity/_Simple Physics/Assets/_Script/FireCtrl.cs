487b9feefaf9d1faa026f125b3f19e230d56781e
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-08-23 09:10:59 +0900

    u

---
+++ _7. unity/_Simple Physics/Assets/_Script/FireCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour {
+
+    public GameObject   _bullet;
+
+    public Transform _firePos;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*
+            GetMouseButton(int button)          -   마우스 버튼을 클릭하고 있을때 계속 발생.
+            GetMouseButtonDown(int button)      -   마우스 버튼을 클릭했을때 한번 발생.
+            GetMouseButtonUp(int button)        -   마우스 버튼을 떼었을때 한번 발생.
+         */
+        if (Input.GetMouseButtonDown(0))        //  0   :   왼쪽
+            Fire();                             //  1   :   우측 
+                                                //  2   :   가운데 버튼
+	}
+
+    void Fire()
+    {
+        Instantiate(_bullet, _firePos.position, _firePos.rotation);
+    }
+}
---
+++ _7. unity/_Simple Physics/Assets/_Script/RemoveBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "BULLET")
+            Destroy(collision.gameObject);
+    }
+
+}
