2ec261b91c8f52fb2d2cc5909ad69c92b989c3c9
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-09-04 12:43:09 +0900

    u

--- _7. unity/_C# For Unity/C# For Unity/Assets/10_배열/3. 2차원 배열/Dim2Array.cs
+++ _7. unity/_C# For Unity/C# For Unity/Assets/10_배열/3. 2차원 배열/Dim2Array.cs
@@ -16,6 +16,10 @@ using UnityEngine;
 
 		-	주로 2차원 배열이 많이 사용됨.
 
+            -   2차원 배열
+
+                -   1차원 배열을 원소로 가지는 배열.
+
 	2 - 1.	Rectangular 배열의 선언 및 초기화.
 
 		-	선언
@@ -34,26 +38,56 @@ using UnityEngine;
 		-	초기화
 
 			1)	new 연산자 사용, 배열 크기 명시.
-	
 				예)	int[ , ] arr = new int [2,3]{ {1,2,3}, {4,5,6} };
 
 			2)	new 연산자 사용, 배열 크기 생략.
-		
 				예)	int[ , ] arr = new int [ , ]{ {1,2,3}, {4,5,6} };
 
 			3)	new 연산자와 배열 크기 모두 생략.
-
 				예)	int[ , ] arr = { {1,2,3}, {4,5,6} };
 
 */
 
-//*
+/*
 public class Dim2Array : MonoBehaviour
  {
 	// Use this for initialization
 	void Start () {
-		
-	}	
+
+        print("---- new 연산자 사용, 배열 크기 명시. ----");
+        int[,] arr1 = new int[2, 3] { {1,2,3}, {4,5,6} };
+        for(int i = 0; i < arr1.GetLength(0); ++i)
+        {
+            for (int j = 0; j < arr1.GetLength(1); ++j)
+            {
+                string tmp = string.Format("arr1[{0}, {1}] = {2}", i, j, arr1[i, j]);
+                print(tmp);
+            }
+        }
+
+        print("---- new 연산자 사용, 배열 크기 생략. ----");
+        int[,] arr2 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+        for (int i = 0; i < arr2.GetLength(0); ++i)
+        {
+            for (int j = 0; j < arr2.GetLength(1); ++j)
+            {
+                string tmp = string.Format("arr1[{0}, {1}] = {2}", i, j, arr2[i, j]);
+                print(tmp);
+            }
+        }
+                
+        print("---- new 연산자와 배열 크기 모두 생략. ----");
+        int[,] arr3 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+        for (int i = 0; i < arr3.GetLength(0); ++i)
+        {
+            for (int j = 0; j < arr3.GetLength(1); ++j)
+            {
+                string tmp = string.Format("arr1[{0}, {1}] = {2}", i, j, arr3[i, j]);
+                print(tmp);
+            }
+        }
+
+    }	
 }
 //*/
 
@@ -66,28 +100,54 @@ public class Dim2Array : MonoBehaviour
 
 			-	가변 배열의 요소로 입력되는 배열은 그 길이가 모두 같을 필요 없음.
 
-			예)	
-				1)
-					int[ ][ ] jagged = new int[3][ ];
+		-   초기화	
 
-					jagged[0] = new int [5]{1,2,3,4,5};
-					jagged[1] = new int []{1,2,3};
-					jagged[2] = {1,2};
+            1)
+				int[ ][ ] jagged = new int[3][ ];
 
-				2)	int[ ][ ] jagged2 = new int [2][ ] 
-					{
-						new int [] {100, 200},
-						new int[3] { 1,2,3}
-					};
+				jagged[0] = new int [5]{1,2,3,4,5};
+				jagged[1] = new int []{1,2,3};
+
+			2)	int[ ][ ] jagged2 = new int [2][ ] 
+				{
+					new int [] {100, 200},
+					new int[3] { 1,2,3}
+				};
 				
 */
 
-/*
+//*
 public class Dim2Array : MonoBehaviour
  {
-	// Use this for initialization
-	void Start () {
-		
-	}	
+    // Use this for initialization
+    void Start() {
+
+        int[][] jagged1 = new int[3][];
+        jagged1[0] = new int[5] { 1, 2, 3, 4, 5 };
+        jagged1[1] = new int[] { 10,20,30 };
+        jagged1[2] = new int[] { 100, 200, 300 };
+
+        foreach(int[] arr in jagged1)
+        {
+            print(string.Format("arr\'s Length : {0}", arr.Length));
+            foreach (int value in arr)
+                print(value);
+        }
+
+
+        int[][] jagged2 = new int[2][]
+        {
+            new int[]{1000, 2000},
+            new int[3]{5,6,7}
+        };
+
+        foreach (int[] arr in jagged2)
+        {
+            print(string.Format("arr\'s Length : {0}", arr.Length));
+            foreach (int value in arr)
+                print(value);
+        }
+
+    }	
 }
 //*/
