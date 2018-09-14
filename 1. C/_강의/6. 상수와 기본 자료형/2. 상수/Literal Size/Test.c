d1e4aaffa7bc2bbc8a32758ef2bc036052f69a0a
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-09-14 12:55:08 +0900

    c

--- 1. C/_강의/6. 상수와 기본 자료형/2. 상수/Literal Size/Test.c
+++ 1. C/_강의/6. 상수와 기본 자료형/2. 상수/Literal Size/Test.c
@@ -12,7 +12,7 @@
 	문자형 상수의 경우 컴파일러에 의해 정수로 변환되므로 int형으로 표현된다.
 
 */
-//*
+/*
 int main(void)
 {
 	printf("literal int size : %d\n", sizeof(10));
@@ -32,12 +32,32 @@ int main(void)
 */
 //*
 #define BULLET_SPEED 20
+
+const int BULLET_COUNT = 10;
+int g_nCurBulletCount = 0;
+
+void Shoot()
+{
+	if (g_nCurBulletCount > BULLET_COUNT)
+//	if (g_nCurBulletCount > 10)
+		return;
+
+	++g_nCurBulletCount;
+}
+
 int main(void)
 {
 	const int BULLET_POW = 100;
+	
 
 	printf("총알의 속도  : %d\n", BULLET_SPEED);
 	printf("총알의 공격력  : %d\n", BULLET_POW);
+
+
+
+
+
+
 
 	return 0;
 }
