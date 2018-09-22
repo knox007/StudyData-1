90590353fdc339f19e0f69fb9bd6c5223cd34020
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-09-22 16:28:28 +0900

    c

--- 1. C/_강의/15. 구조체/Struct/Test.c
+++ 1. C/_강의/15. 구조체/Struct/Test.c
@@ -4,31 +4,46 @@
 #include <math.h>
 //======================================================
 /*
-1.	구조체	
+1.	구조체 선언과 접근
+
+		-	선언
+
+			-	struct	구조체타입  객체이름;
+
+		-	멤버변수 접근 방법.
+
+			-	구조체_변수_이름.멤버변수
 */
 
+struct SPoint
+{
+	int _xPos;
+	int _yPos;
+};
+
+struct SPerson
+{
+	char _name[30];
+	char _phoneNumber[20];
+	int _age;
+};
 
 /*
-struct point
-{
-	int xPos;
-	int yPos;
-};
 
 int main()
 {
-	struct point pos1, pos2;
+	struct SPoint pos1, pos2;
 	double distance;
 
 	printf("첫번째 위치 정보를 입력하세요 : ");
-	scanf("%d %d", &pos1.xPos, &pos1.yPos);
+	scanf("%d %d", &pos1._xPos, &pos1._yPos);
 
 	printf("두번째 위치 정보를 입력하세요 : ");
-	scanf("%d %d", &pos2.xPos, &pos2.yPos);
+	scanf("%d %d", &pos2._xPos, &pos2._yPos);
 
 	distance = sqrt( (double)
-		((pos1.xPos - pos2.xPos)*(pos1.xPos - pos2.xPos) +
-		(pos1.yPos - pos2.yPos)*(pos1.yPos - pos2.yPos)));
+		((pos1._xPos - pos2._xPos)*(pos1._xPos - pos2._xPos) +
+		(pos1._yPos - pos2._yPos)*(pos1._yPos - pos2._yPos)));
 	
 
 	printf("두 점 사이 거리 : %g\n", distance);
@@ -38,76 +53,51 @@ int main()
 
 //*/
 //======================================================
-
 /*
-	quiz)
-	이름과 전화번호, 나이를 관리하는 구조체를 만들고
-	scanf로 정보를 입력받아 다음과 같이 출력하도록 만든다.
-
-	이름		: 홍길동
-	전화번호	: 010-xxxx-xxxxx
-	나이		: 20
-
-	홍길동씨의 나이는 20세이며 전화번호는 010-xxxx-xxxx 입니다.
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-struct person
-{
-	char name[30];
-	char phoneNumber[20];
-	int age;
-};
-
-
 int main()
 {
-	struct person tmpPerson;
+	struct SPerson tmpPerson1,tmpPerson2;
+
+	strcpy(tmpPerson1._name, "홍길동");
+	strcpy(tmpPerson1._phoneNumber, "010-1234-5678");
+	tmpPerson1._age = 20;
+	printf("%s씨의 나이는 %d세 이며 전화번호는 %s 입니다.\n", tmpPerson1._name, tmpPerson1._age, tmpPerson1._phoneNumber);
 	
 	printf("이름 : ");
-	scanf("%s", tmpPerson.name);
+	scanf("%s", tmpPerson2._name);
 
 	printf("전화번호 : ");
-	scanf("%s", tmpPerson.phoneNumber);
+	scanf("%s", tmpPerson2._phoneNumber);
 
 	printf("나이 : ");
-	scanf("%d", &tmpPerson.age);
+	scanf("%d", &tmpPerson2._age);
 
-	printf("%s씨의 나이는 %d세 이며 전화번호는 %s 입니다.\n", tmpPerson.name, tmpPerson.age, tmpPerson.phoneNumber);
+	printf("%s씨의 나이는 %d세 이며 전화번호는 %s 입니다.\n", tmpPerson2._name, tmpPerson2._age, tmpPerson2._phoneNumber);
 
 
 	return 0;
 }
 //*/
+//======================================================
+/*
+2.	구조체 초기화
+
+	-	배열의 초기화와 유사.
+
+*/
+
+/*
+int main()
+{
+	struct SPoint pos = { 10, 20 };
+
+	struct SPerson man = { "홍길동", "010-1234-5678", 20 };
+
+	printf("%d, %d\n\n", pos._xPos, pos._yPos);
+
+	printf("%s, %s, %d\n\n", man._name, man._phoneNumber, man._age);
+
+	return 0;
+}
+//*/
+//======================================================
