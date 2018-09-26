3142927f47d7c00dc0825de3eb1c8bfba807f5eb
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-09-26 18:39:51 +0900

    c

--- 1. C/_강의/15. 구조체/2. 구조체와 배열, 포인터/2. 구조체 포인터/Struct Pointer/Test.c
+++ 1. C/_강의/15. 구조체/2. 구조체와 배열, 포인터/2. 구조체 포인터/Struct Pointer/Test.c
@@ -13,18 +13,19 @@
 
 /*
 
-struct SPoint
+typedef struct
 {
 	int _xPos;
 	int _yPos;
-};
+
+}SPoint;
 
 int main()
 {
-	struct SPoint pos1 = { 1,2 };
-	struct SPoint pos2 = { 100, 200 };
+	SPoint pos1 = { 1,2 };
+	SPoint pos2 = { 100, 200 };
 	
-	struct SPoint *pPos3 = &pos1;
+	SPoint *pPos3 = &pos1;
 	(*pPos3)._xPos += 4;
 	(*pPos3)._yPos += 5;
 	printf("[%d, %d]\n", pPos3->_xPos, pPos3->_yPos);
@@ -43,25 +44,55 @@ int main()
 	-	멤버변수로 포인터 사용하기.
 */
 //======================================================
-//*
-//	1)	다른 타입의 포인터 변수.
+/*
+//	1)	자신과 다른 타입의 포인터 변수.
 
-struct SPoint
+typedef struct 
 {
 	int _xPos;
 	int _yPos;
-};
 
-struct SCircle
+}SPoint;
+
+typedef struct 
 {
 	double _rad;
-	struct SPoint * _pCenter;
-};
+	SPoint * _pCenter;
+
+}SCircle;
 
 int main()
 {
+	SPoint center = { 1,1 };
+	double rad = 5;
+
+	SCircle circle = { rad, &center };
+
+	printf("원의 반지름 : %f\n", circle._rad);
+	printf("원의 중심 : [%d, %d]\n", circle._pCenter->_xPos, circle._pCenter->_yPos);
+
 	
 
 	return 0;
 }
+//*/
+
+
+//*
+//	2)	자신과 같은 타입의 포인터 변수.
+typedef struct
+{
+	int _xPos;
+	int _yPos;
+	SPoint *pPoint;
+
+}SPoint;
+
+int main()
+{
+
+
+	return 0;
+}
+
 //*/
