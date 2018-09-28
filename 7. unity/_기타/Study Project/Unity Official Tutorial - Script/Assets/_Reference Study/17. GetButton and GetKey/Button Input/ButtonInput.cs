a226f36783235e0519fd80543f6fd769d38016b8
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-09-28 15:33:19 +0900

    u

---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/17. GetButton and GetKey/Button Input/ButtonInput.cs
@@ -0,0 +1,51 @@
+﻿//=====================================================
+/*
+    Edit - ProjectSetting - Input
+*/
+//=====================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//=====================================================
+public class ButtonInput : MonoBehaviour
+{
+    public Text _buttonState;
+
+    void Update()
+    {
+        //  public static bool GetButtonDown(string buttonName);
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+
+        if (down)
+        {
+            _buttonState.text = "Jump Down";
+            _buttonState.color = Color.blue;
+            _buttonState.rectTransform.localPosition = Vector3.up * 50;
+        }
+        else if (held)
+        {
+            _buttonState.text = "Jump Held";
+            _buttonState.color = Color.yellow;
+            _buttonState.rectTransform.localPosition = Vector3.right * 50;
+
+        }
+        else if (up)
+        {
+            _buttonState.text = "Jump Up";
+            _buttonState.color = Color.red;
+            _buttonState.rectTransform.localPosition = Vector3.down * 50;
+        }
+        else
+        {
+            _buttonState.text = "No Event";
+            _buttonState.color = Color.white;
+            _buttonState.rectTransform.localPosition = Vector3.zero;
+        }
+
+    }// void Update()
+
+}// public class ButtonInput : MonoBehaviour
+//=====================================================
---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/18. GetAxis/1. AxisExample/AxisExample.cs
@@ -0,0 +1,32 @@
+﻿//=====================================================
+/*
+    Edit - ProjectSetting - Input
+*/
+//=====================================================
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//=====================================================
+public class AxisExample : MonoBehaviour
+{
+    public float range;
+    public Text textOutput;
+
+
+    void Update()
+    {
+        /*  public static float GetAxis (string axisName); 
+        
+            -   키 입력에 따라 -1.0 ~ 1.0 의 값을 반환.
+
+            -   입력 없으면 0.
+
+        */
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
+    }
+}
+//=====================================================
