3f552e008c6f8a9ba21814e38053965851468981
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-10-03 18:51:03 +0900

    u

---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/0. Classes( 23 )/_script/기능을 단일 클래스로 집중/SingleCharacterScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCharacterScript : MonoBehaviour {
+
+	public class Stuff
+	{
+		public int bullets;
+		public int grenades;
+		public int rockets;
+
+		public Stuff(int bul, int gre, int roc)
+		{
+			bullets = bul;
+			grenades = gre;
+			rockets = roc;
+		}
+	}
+
+	public int bulletCount = 10;
+	public Stuff myStuff;
+	public float speed;
+	public float turnSpeed;
+	public Rigidbody bulletPrefab;
+	public Transform firePosition;
+	public float bulletSpeed;
+
+	void Start()
+	{
+		Debug.Log ("--- SingleCharacterScript ---");
+		myStuff = new Stuff(bulletCount, 7, 25);
+		Debug.Log(myStuff.bullets); 
+	}
+
+	void Update ()
+	{
+		Movement();
+		Shoot();
+	}
+
+
+	void Movement ()
+	{
+		float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+		transform.Translate(Vector3.forward * forwardMovement);
+		transform.Rotate(Vector3.up * turnMovement);
+	}
+
+
+	void Shoot ()
+	{
+		if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+		{
+			Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+			bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+			myStuff.bullets--;
+		}
+	}
+}
---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/24. Instantiate/_script/RocketDestruction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketDestruction : MonoBehaviour {
+
+	public float _destroyTime = 3.0f;
+
+	void Start()
+	{
+		Destroy (gameObject, _destroyTime);
+	}
+}
