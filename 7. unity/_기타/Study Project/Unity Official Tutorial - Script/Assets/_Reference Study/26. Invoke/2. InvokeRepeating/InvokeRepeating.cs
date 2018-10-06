fc61489494a16ed705d5402fbd8ec70a78aafd14
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-10-06 19:44:12 +0900

    u

---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/26. Invoke/2. InvokeRepeating/InvokeRepeating.cs
@@ -0,0 +1,35 @@
+﻿/*
+	InvokeRepeating
+
+	-	pubic void InvokeRepeating( string methodName, float time, float repeatRate );
+
+		-	methodName	:	실행할 메소드.
+
+		-	time		:	호출 지연시간.
+
+		-	repeatRate	:	호출 반복시간.
+
+		-	time 후에 methodName 를 호출한 후 repeatRate 마다
+
+			반복적으로 호출.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeating : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start()
+	{
+		InvokeRepeating("SpawnObject", 2, 1);
+	}
+
+	void SpawnObject()
+	{
+		float x = Random.Range(-2.0f, 2.0f);
+		float z = Random.Range(-2.0f, 2.0f);
+		Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+	}
+}
---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/26. Invoke/4. IsInvoking/IsInvoking.cs
@@ -0,0 +1,45 @@
+﻿/*
+	IsInvoking
+
+	-	public bool IsInvoking( string methodName );
+
+		-	methodName에대한 Invoke 호출이 있는지 확인.
+
+	-	public bool IsInvoking();
+
+		-	Invoke 호출이 있는지 확인.
+		
+
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsInvoking : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start()
+	{
+		
+	}
+
+	void SpawnObject()
+	{
+		float x = Random.Range(-2.0f, 2.0f);
+		float z = Random.Range(-2.0f, 2.0f);
+		Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+	}
+
+	void Update()
+	{
+		//if (Input.GetButton ("Jump") && !IsInvoking("SpawnObject"))
+		if (Input.GetButton ("Jump") && !IsInvoking ())
+			InvokeRepeating ("SpawnObject", 1, 1);
+
+		//if (IsInvoking ("SpawnObject"))
+		if (IsInvoking ())
+			print ("SpawnObject Invoking!!!!");
+			
+	}
+}
