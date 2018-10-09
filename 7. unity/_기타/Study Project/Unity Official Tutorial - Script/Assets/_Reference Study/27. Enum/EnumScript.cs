866d0514c65e5573517ed7cae44cd47af577b407
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-10-09 17:47:05 +0900

    u

---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/27. Enum/EnumScript.cs
@@ -0,0 +1,81 @@
+﻿/*
+	열거형
+
+	-	선언.
+
+		-	enum 열거형이름
+			{
+				열거형상수1,
+				열거형상수2,
+				열거형상수3,
+				.......
+			};
+
+
+
+	-	특징.
+	
+		-   정수형 상수의 일종.
+
+	    -   특별히 값을 정의하지 않아도 자동으로 0부터 1씩 증가하며
+
+    	    값이 적용됨.
+
+	    -   값을 정의하게 되면 다음 상수부터는 그 값을 기준으로
+
+    	    1씩 증가된 값이 적용.
+
+		-	정수형 상수이므로 if, switch, for 등에 적용 가능.
+
+	
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumScript : MonoBehaviour {
+
+	public enum eDirection
+	{
+		East,
+		West,
+		South,
+		North,
+	};
+
+	public eDirection _curDir;
+    eDirection _preDir;
+
+	void Start () 
+	{
+		print(_curDir);
+		print((int)_curDir);
+		
+        _preDir = _curDir;
+    }
+
+    void Update()
+    {
+        if( _curDir != _preDir)
+        {
+            print(_curDir);
+			print((int)_curDir);
+
+            _preDir = _curDir;
+        }
+    }
+
+	eDirection ReverseDirection (eDirection dir)
+	{
+		if(dir == eDirection.North)
+			dir = eDirection.South;
+		else if(dir == eDirection.South)
+			dir = eDirection.North;
+		else if(dir == eDirection.East)
+			dir = eDirection.West;
+		else if(dir == eDirection.West)
+			dir = eDirection.East;
+
+		return dir;     
+	}
+}
---
+++ 7. unity/_기타/Study Project/Unity Official Tutorial - Script/Assets/_Reference Study/28. Switch/ConversationScript.cs
@@ -0,0 +1,136 @@
+﻿/*
+	switch
+
+	-	형식
+
+		switch( 조건값 )
+		{
+			case 데이터 1:
+				......
+				break;
+
+
+			case 데이터 2:			
+				......
+				break;
+
+
+			case 데이터 3:
+				......
+				break;
+
+			default:
+				......
+				break;
+		}
+	-	조건값은 int/enum/string 가능.
+
+		(c/c++은 정수만(char 포함.) 가능.)
+
+	-	break문이 없으면 다음 break를 만날때까지 모두 처리.
+
+	-	if ~ else문과 유사.
+
+		차이점
+
+		-	if ~ else는 조건의 범위가 넓은 경우 유용.
+
+		-	switch는 조건의 범위가 한가지 이거나 
+
+			중복처리해도 무관한 경우.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationScript : MonoBehaviour {
+
+	public enum eFOOD
+	{
+		APPLE,
+		BANANA,
+		CHOCOLATE,
+		DONUT,
+		GRAPE
+	};
+
+	public eFOOD _curFood;
+	eFOOD _preFood;
+
+	void Start()
+	{
+		_curFood = eFOOD.APPLE;
+		//_curFood = (eFOOD)100;
+		ShowFood (_curFood);
+		_preFood = _curFood;
+
+
+		//	ShowFood ("사과");
+	}
+
+	void Update()
+	{
+		if (_curFood != _preFood)
+		{
+			ShowFood (_curFood);
+			_preFood = _curFood;
+		}
+	}
+
+	void ShowFood( string foodName )
+	{
+		switch( foodName )
+		{
+		case "사과":
+			print ("사과");
+			break;
+
+		case "딸기":
+			print ("딸기");
+			break;
+
+		case "포도":
+			print ("포도");
+			break;
+
+		case "복숭아":
+			print ("복숭아");
+			break;
+
+		default:
+			print ("과일이 아닙니다.");
+			break;
+		}
+
+	}
+	void ShowFood( eFOOD eFood )
+	{
+		switch (eFood)
+		{
+		case eFOOD.APPLE:
+			print ("APPLE");
+			break;
+
+		case eFOOD.BANANA:
+			print ("BANANA");
+			break;
+
+		case eFOOD.CHOCOLATE:
+			print ("CHOCOLATE");
+			break;
+
+		case eFOOD.DONUT:
+			print ("DONUT");
+			break;
+
+		case eFOOD.GRAPE:
+			print ("GRAPE");
+			break;
+
+		default:
+			print ("this is not food!!!!");
+			break;
+
+		}
+	}
+}
