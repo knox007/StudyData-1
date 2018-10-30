943c750bf054dd1fb4703fa574b3e03d294e6ec2
Jackie_Jina\Jackie & Jina <jackielee0153@gmail.com>
2018-10-30 14:43:34 +0900

    stl

---
+++ 2. C++/6. STL/2. List/Test.cpp
@@ -0,0 +1,85 @@
+//=================================================
+#include<iostream>
+using namespace std;
+#pragma warning( disable : 4996 )
+//=================================================
+/*
+	---------------
+	1.	List
+	---------------
+
+		-	순차 컨테이너.
+
+		-	이중 링크드 리스트 구조.
+
+		-	삽입, 삭제가 빈번한 경우 추천.
+
+			-	모든 컨테이너중 가장 빠름.
+
+		-	임의 접근 반복자( at(), [] )는 사용 불가.
+
+*/
+//=================================================
+//*
+#include<list>
+
+bool IsRangeOK(int num)
+{
+	return 100 <= num && num <= 200;
+}
+
+int main()
+{	
+	list<int> lt;
+
+	//	추가.
+	lt.push_back(10);
+	lt.push_back(20);
+	lt.push_back(108);
+	lt.push_back(60);
+	lt.push_back(10);
+	lt.push_back(100);
+	lt.push_back(40);
+	lt.push_back(50);
+	lt.push_back(10);
+	lt.push_back(109);
+	lt.push_back(30);
+	lt.push_back(220);
+	lt.push_back(10);
+
+	list<int>::iterator iterCur;
+
+	cout << "--- 조회 ---" << endl;
+	for (iterCur = lt.begin(); iterCur != lt.end(); iterCur++) {
+		cout << *iterCur << " ";
+	}
+	cout << endl << endl;
+
+
+	cout << "--- 조건부 삭제 1 ---" << endl;
+	lt.remove_if(IsRangeOK);
+	for (iterCur = lt.begin(); iterCur != lt.end(); iterCur++) {
+		cout << *iterCur << " ";
+	}
+	cout << endl << endl;
+
+
+	cout << "--- 조건부 삭제 2 ---" << endl;
+	lt.remove(10);
+	for (iterCur = lt.begin(); iterCur != lt.end(); iterCur++) {
+		cout << *iterCur << " ";
+	}
+	cout << endl << endl;
+
+
+	return 0;
+
+}//	int main()
+//*/
+//=================================================
+/*
+	Quiz)	주소록 관리 프로그램을 list로 만든다.
+
+			등록, 삭제, 조회, 전체 삭제 기능만 적용.
+*/
+//=================================================
