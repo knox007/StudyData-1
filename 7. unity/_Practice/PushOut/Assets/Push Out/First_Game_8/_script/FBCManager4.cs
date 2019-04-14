9b64d217ee7908933483fe2e2004d9c10006907c
jackielee <jackielee2@nate.com>
2019-04-14 23:56:24 +0900

    갱신

---
+++ 7. unity/_Practice/PushOut/Assets/Push Out/First_Game_8/_script/FBCManager4.cs
@@ -0,0 +1,60 @@
+﻿//==============================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//==============================================================
+public class FBCManager4 : MonoBehaviour
+{
+    //----------------------------
+    public UIManager3 _uiManager;
+    //----------------------------
+    public int _count;      //  낙하하는 박스 갯수 파악.
+    public float _time;     //  박스가 떨어지기까지 걸리는 시간 파악.
+    bool _isEnd;            //  박스가 다 떨어졌는지 확인.
+    //----------------------------
+    private void Start()
+    {
+        //  초기화.
+        _count = 0;
+        _isEnd = false;
+        _time = 0f;
+    }
+    //----------------------------
+    private void OnTriggerEnter(Collider other)
+    {
+        //  충돌한 오브젝트의 태그를 확인한다.
+        if( other.gameObject.CompareTag("FBC"))
+            ++_count;
+
+        //  플레이어가 떨어지면 실패로 처리한다.
+        if (other.gameObject.CompareTag("Player") && _isEnd == false)
+        {
+            _isEnd = true;
+            _uiManager._textFailed.gameObject.SetActive(true);
+            _uiManager._imageBG.gameObject.SetActive(true);
+        }
+
+
+        //  16개 이상 떨어지면 박스가 다 떨어진것으로 한다.
+        //  이 경우, 게임 성공으로 처리한다.
+        if (_count >= 16)
+        {            
+            _isEnd = true;
+            _uiManager._textClear.gameObject.SetActive(true);
+            _uiManager._imageBG.gameObject.SetActive(true);
+        }
+    }
+    //----------------------------
+    private void Update()
+    {
+        //  박스가 다 떨어지지 않은 상태라면 경과시간을 누적한다.
+        if (_isEnd==false)
+            _time += Time.deltaTime;
+
+        _uiManager._textTime.text = _time.ToString();
+    }
+    //----------------------------
+
+
+}// public class FBCManager : MonoBehaviour
+ //==============================================================
---
+++ 7. unity/_Practice/PushOut/Assets/Push Out/First_Game_8/_script/UIManager3.cs
@@ -0,0 +1,25 @@
+﻿//========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   //  UI관련 기능을 사용하기 위한 네임스페이스.
+//========================================================
+public class UIManager3 : MonoBehaviour
+{
+    //---------------------
+    public Text _textTime;      //  경과시간을 연출하는 텍스트UI.
+    public Text _textClear;     //  성공 텍스트.
+    public Text _textFailed;    //  실패 텍스트.
+    public Image _imageBG;      //  결과 텍스트의 배경으로 사용될 이미지.
+    //---------------------
+    private void Start()
+    {
+        //  처음 시작할때는 메시지를 보여줄 필요가 없다.
+        _textClear.gameObject.SetActive(false);
+        _textFailed.gameObject.SetActive(false);
+        _imageBG.gameObject.SetActive(false);
+    }
+    //---------------------
+
+}// public class UIManager2 : MonoBehaviour
+ //========================================================
