e6c29c36ecb46a1bb97136dba4814d470135bb82
jackielee <jackielee2@nate.com>
2019-04-14 22:10:45 +0900

    갱신

---
+++ 7. unity/_Practice/PushOut/Assets/Push Out/First_Game_5/_script/FBCManager.cs
@@ -0,0 +1,42 @@
+﻿//==============================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//==============================================================
+public class FBCManager : MonoBehaviour
+{
+    //----------------------------
+    public int _count;      //  낙하하는 박스 갯수 파악.
+    public float _time;     //  박스가 떨어지기까지 걸리는 시간 파악.
+    bool _isEnd;            //  박스가 다 떨어졌는지 확인.
+    //----------------------------
+    private void Start()
+    {
+        //  초기화.
+        _count = 0;
+        _isEnd = false;
+        _time = 0f;
+    }
+    //----------------------------
+    private void OnTriggerEnter(Collider other)
+    {
+        //  충돌한 오브젝트의 태그를 확인한다.
+        if( other.gameObject.CompareTag("FBC"))
+            ++_count;
+
+        //  16개 이상 떨어지면 박스가 다 떨어진것으로 한다.
+        if (_count >= 16)
+            _isEnd = true;
+    }
+    //----------------------------
+    private void Update()
+    {
+        //  박스가 다 떨어지지 않은 상태라면 경과시간을 누적한다.
+        if (_isEnd==false)
+            _time += Time.deltaTime;
+    }
+    //----------------------------
+
+
+}// public class FBCManager : MonoBehaviour
+ //==============================================================
