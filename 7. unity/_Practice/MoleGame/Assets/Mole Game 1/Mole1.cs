2f22307b149c1e7a65c82e8801280abfde2db6d6
jackielee <jackielee2@nate.com>
2019-04-23 00:37:27 +0900

    두더지 잡기 추가 중.

---
+++ 7. unity/_Practice/MoleGame/Assets/Mole Game 1/Mole1.cs
@@ -0,0 +1,22 @@
+﻿//=================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//=================================================================
+public class Mole1 : MonoBehaviour
+{
+    //-----------------------
+    //  두더지 상태 선언.
+    public enum eMOLESTATE
+    {
+        NONE,
+        OPEN,
+        IDLE,
+        CLOSE,
+        CATCH
+    }
+    public eMOLESTATE _eMoleState;
+    //-----------------------
+
+}// public class Mole1 : MonoBehaviour
+ //=================================================================
